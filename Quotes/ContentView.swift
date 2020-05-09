441a4a5f74e736819b818f9a5306e3017e499acc
Roger Spielmann <drummer23@braveheart.ch>
2020-05-09 20:49:10 +0200

    Initial Commit

---
+++ Quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quotes
+//
+//  Created by Roger Spielmann on 09.05.20.
+//  Copyright © 2020 LiveCrew. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
